62ed5713395438a30e5abf552fae2e86ea6bd590
dxmekch <dominik.kauer@gmail.com>
2018-07-14 22:20:07 +0200

    Create cd3.py

---
+++ bindings/python/samples/cd3.py
@@ -0,0 +1,22 @@
+  GNU nano 2.2.6                                                             File: ct3.py                                                                                                                                  
+import sys                                              # args
+import os                                               # files, directories
+from datetime import datetime, timedelta, date                          # time formats
+import datetime as dt
+
+countdown = timedelta(seconds = 5)
+now = datetime.now()
+savetime = now + countdown
+
+print datetime.now()
+while time_left != 0:
+  time_left = savetime - now
+  print time_left('%Y-%m-%d')
+
+exit()
+
+
+
+
+
+
